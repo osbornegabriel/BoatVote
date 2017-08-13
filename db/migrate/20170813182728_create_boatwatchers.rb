c4e4578dcbcb3e7ca173f1bb66a8a11103c260e9
Gabriel Osborne <osbornegabriel@gmail.com>
2017-08-13 18:39:11 -0500

    Write migrations

--- db/migrate/20170813182728_create_boatwatchers.rb
+++ db/migrate/20170813182728_create_boatwatchers.rb
@@ -1,4 +1,11 @@
 class CreateBoatwatchers < ActiveRecord::Migration
   def change
+    create_table :boatwatchers do |t|
+      t.string :username, null: false
+      t.string :email, null: false, unique: true
+      t.string :password_hash, null: false
+
+      t.timestamps
+    end
   end
 end
--- db/migrate/20170813182746_create_boatvotes.rb
+++ db/migrate/20170813182746_create_boatvotes.rb
@@ -1,4 +1,14 @@
 class CreateBoatvotes < ActiveRecord::Migration
   def change
+    create_table :boatvotes do |t|
+      t.integer :voter_id, null: false
+      t.integer :boat_id, null: false
+      t.boolean :vote, default: nil
+
+      t.timestamps
+    end
   end
 end
+
+
+# With line 6, (the vote), I'm going to experiment with a 3-value-logic. With SQL I read articles discussing UNKNOWN as a value in boolean table-rows. Many advise against this, others think it's the best thing ever. This might blow up in my face - let's find out!
