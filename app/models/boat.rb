cd7b0974804e4093064f333b86695229c4b629d8
Gabriel Osborne <osbornegabriel@gmail.com>
2017-08-13 20:07:31 -0500

    Add validations to boat_vote and boat models

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -3,4 +3,6 @@ class Boat < ActiveRecord::Base
   belongs_to :discoverer, class_name: BoatWatcher
   has_many :boat_votes
   has_many :voters, through: :boat_votes, source: :voter
+
+  validates_presence_of :url, :discoverer_id
 end
--- app/models/boat_vote.rb
+++ app/models/boat_vote.rb
@@ -2,4 +2,6 @@ class BoatVote < ActiveRecord::Base
   # Remember to create a migration!
   belongs_to :voter, class_name: "BoatWatcher", foreign_key: :voter_id
   belongs_to :boat
+
+  validates_presence_of :voter_id, :boat_id
 end
